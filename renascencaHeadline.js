0fcc2dfb89bf09176fa93e0d2aa245891f5634d4
Alex Santos <alexandre.santos@publico.pt>
2024-03-03 20:13:55 +0000

    add renascenca

---
+++ renascencaHeadline.js
@@ -0,0 +1,25 @@
+const rp = require('request-promise');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://rr.sapo.pt';
+
+rp(url)
+  .then(function(html){
+    const $ = cheerio.load(html);
+
+    const articles = [];
+    const articleTitle = $('.column-group.bottom-space a h3', html).eq(0);
+    const articleUrl = $('.column-group.bottom-space .all-100:last-child a');
+    articles.push(articleTitle.text().trim());
+    articles.push('https://rr.sapo.pt/' + articleUrl[0].attribs.href);
+    console.log(articles);
+
+    const jsonString = JSON.stringify(Object.assign({}, articles))
+    fs.writeFile('headline--renascenca.json', jsonString, function(err){
+      console.log('File successfully written');
+    });
+
+  })
+  .catch(function(err){
+    console.log(err);
+  });
