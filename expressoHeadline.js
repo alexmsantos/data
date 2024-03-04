81a9b53e9aa683b749cf47548e4d672b7e07413b
Alex Santos <alexandre.santos@publico.pt>
2024-03-04 09:41:32 +0000

    add expresso

--- expressoHeadline.js
+++ expressoHeadline.js
@@ -9,16 +9,16 @@ rp(url)
 
     const articles = [];
     const articleTitle = $('.main-section .teaser-article .text-details > h2.title > a', html).eq(0);
-    //const articleUrl = $('.editorial-grid .mod h1.title a');
+    //const articleUrl = $('.main-section .teaser-article .text-details > h2.title > a');
     articles.push(articleTitle.text());
     //articles.push(articleUrl[0].attribs.href);
     console.log(articles);
-/*
+
     const jsonString = JSON.stringify(Object.assign({}, articles))
     fs.writeFile('headline--expresso.json', jsonString, function(err){
       console.log('File successfully written');
     });
-*/
+
   })
   .catch(function(err){
     console.log(err);
