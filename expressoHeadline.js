6666a021068e4f485842aa482886f1aeb8f7770e
Alex Santos <alexandre.santos@publico.pt>
2024-03-04 10:09:51 +0000

    add expresso with puppeteer

--- expressoHeadline.js
+++ expressoHeadline.js
@@ -1,17 +1,25 @@
-const rp = require('request-promise');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 const cheerio = require('cheerio');
 const url = 'https://expresso.pt';
 
-rp(url)
-  .then(function(html){
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
     const $ = cheerio.load(html);
-
     const articles = [];
     const articleTitle = $('.main-section .teaser-article .text-details > h2.title > a', html).eq(0);
-    //const articleUrl = $('.main-section .teaser-article .text-details > h2.title > a');
+    const articleUrl = $('.main-section .teaser-article .text-details > h2.title > a');
     articles.push(articleTitle.text());
-    //articles.push(articleUrl[0].attribs.href);
+    articles.push('https://expresso.pt/' + articleUrl[0].attribs.href);
     console.log(articles);
 
     const jsonString = JSON.stringify(Object.assign({}, articles))
@@ -20,6 +28,6 @@ rp(url)
     });
 
   })
-  .catch(function(err){
+  .catch(function(err) {
     console.log(err);
   });
