fd48e04ed0d6a83532246363747612ee22496bf5
Alex Santos <alexandre.santos@publico.pt>
2024-03-04 11:03:54 +0000

    add sic noticias

--- rtpHeadline.js
+++ rtpHeadline.js
@@ -11,7 +11,7 @@ rp(url)
     const articleTitle = $('.main-article h2.main-title', html).eq(0);
     const articleUrl = $('.main-article > a');
     articles.push(articleTitle.text().trim());
-    articles.push(articleUrl[0].attribs.href);
+    articles.push(articleUrl[0].attribs.href.trim());
     console.log(articles);
 
     const jsonString = JSON.stringify(Object.assign({}, articles))
--- sicHeadline.js
+++ sicHeadline.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const cheerio = require('cheerio');
-const url = 'https://expresso.pt';
+const url = 'https://sicnoticias.pt';
 
 puppeteer
   .launch()
@@ -19,11 +19,11 @@ puppeteer
     const articleTitle = $('.main-section .teaser-article .text-details > h2.title > a', html).eq(0);
     const articleUrl = $('.main-section .teaser-article .text-details > h2.title > a');
     articles.push(articleTitle.text());
-    articles.push('https://expresso.pt' + articleUrl[0].attribs.href);
+    articles.push('https://sicnoticias.pt' + articleUrl[0].attribs.href);
     console.log(articles);
 
     const jsonString = JSON.stringify(Object.assign({}, articles))
-    fs.writeFile('headline--expresso.json', jsonString, function(err){
+    fs.writeFile('headline--sicnoticias.json', jsonString, function(err){
       console.log('File successfully written');
     });
 
