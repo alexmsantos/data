bb74a5c4bbbfde22dd0a7eed712fcd8045a06749
Alex Santos <alexandre.santos@publico.pt>
2024-03-04 21:29:27 +0000

    add brands

---
+++ dinheirovivoHeadline.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.dinheirovivo.pt';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('.component-root h2.title', '.component-root a.clean-link');
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articles = [];
+    const articleTitle = $('.component-root h2.title');
+    const articleUrl = $('.component-root a.clean-link');
+    articles.push(articleTitle.first().text().trim());
+    articles.push('https://www.dinheirovivo.pt' + articleUrl[0].attribs.href);
+    console.log(articles);
+
+    const jsonString = JSON.stringify(Object.assign({}, articles))
+    fs.writeFile('headline--dinheirovivo.json', jsonString, function(err){
+      console.log('File successfully written');
+      // Exit the process after the file is written
+      process.exit(0);
+    });
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
---
+++ negociosHeadline.js
@@ -0,0 +1,25 @@
+const rp = require('request-promise');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.jornaldenegocios.pt';
+
+rp(url)
+  .then(function(html){
+    const $ = cheerio.load(html);
+
+    const articles = [];
+    const articleTitle = $('article.destaque h1 > a');
+    const articleUrl = $('article.destaque h1 > a');
+    articles.push(articleTitle[0].attribs.title);
+    articles.push('https://www.jornaldenegocios.pt' + articleUrl[0].attribs.href);
+    console.log(articles);
+
+    const jsonString = JSON.stringify(Object.assign({}, articles))
+    fs.writeFile('headline--negocios.json', jsonString, function(err){
+      console.log('File successfully written');
+    });
+
+  })
+  .catch(function(err){
+    console.log(err);
+  });
