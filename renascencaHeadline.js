0a7249a59f7619685775324fda22aa078022b412
Alex Santos <alexandre.santos@publico.pt>
2024-03-05 16:17:22 +0000

    add date to renascenca

--- renascencaHeadline.js
+++ renascencaHeadline.js
@@ -12,6 +12,9 @@ rp(url)
     const articleUrl = $('.column-group.bottom-space .all-100:last-child a');
     articles.push(articleTitle.text().trim());
     articles.push('https://rr.sapo.pt/' + articleUrl[0].attribs.href);
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+    articles.push(dateIso);
     console.log(articles);
 
     const jsonString = JSON.stringify(Object.assign({}, articles))
