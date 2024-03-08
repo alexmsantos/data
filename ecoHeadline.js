e8d766f8987155aa276bb002f7ea099c270342c8
Alex Santos <alexandre.santos@publico.pt>
2024-03-08 19:44:47 +0000

    Update ecoHeadline.js

--- ecoHeadline.js
+++ ecoHeadline.js
@@ -7,20 +7,23 @@ rp(url)
   .then(function(html){
     const $ = cheerio.load(html);
 
-    const articles = [];
     const articleTitle = $('article.highlight .highlight__content h2.title').eq(0);
     const articleUrl = $('article.highlight .highlight__content a');
-    articles.push(articleTitle.text().trim());
-    articles.push(articleUrl[0].attribs.href);
     let ms = new Date();
     const dateIso = ms.toISOString()
-    articles.push(dateIso);
-    console.log(articles);
 
-    const jsonString = JSON.stringify(Object.assign({}, articles))
-    fs.writeFile('headline--eco.json', jsonString, function(err){
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.text().trim(),
+      url: articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "ECO"
+    }));
+
+    fs.writeFile('headlines/headline--eco.json', jsonString, function(err){
       console.log('File successfully written');
     });
+
+    console.log(jsonString);
 
   })
   .catch(function(err){
