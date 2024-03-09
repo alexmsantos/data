fabd799d62cad5455901da9d6566adf1c6bec6af
Alex Santos <alexandre.santos@publico.pt>
2024-03-09 15:04:06 +0000

    add nytimes

---
+++ headline--nytimes.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.nytimes.com';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('section.story-wrapper p.indicate-hover', 'section.story-wrapper > a', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articleTitle = $('section.story-wrapper p.indicate-hover');
+    const articleUrl = $('section.story-wrapper > a');
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.first().text().trim(),
+      url: articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "The New York Times",
+    }));
+
+    fs.writeFile('headlines-world/headline--nytimes.json', jsonString, function(err){
+      console.log('File successfully written');
+      process.exit(0);
+    });
+    
+    console.log(jsonString);
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
