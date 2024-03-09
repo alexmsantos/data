7d60c9063fba569e84639b90117aaf1a274e3dc8
Alex Santos <alexandre.santos@publico.pt>
2024-03-09 15:31:34 +0000

    add wapo

---
+++ headline--wapo.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.washingtonpost.com';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url, { timeout: 90000 }).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('.headline > h2 > a > span', '.headline > h2 > a', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articleTitle = $('.headline > h2 > a > span');
+    const articleUrl = $('.headline > h2 > a');
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.first().text().trim(),
+      url: articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "The Washington Post",
+    }));
+
+    fs.writeFile('headlines-world/headline--wapo.json', jsonString, function(err){
+      console.log('File successfully written');
+      process.exit(0);
+    });
+    
+    console.log(jsonString);
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
