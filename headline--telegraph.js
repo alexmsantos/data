1a4013146c49a8d5bcdf8cd0cf0747e5cf9bf7e8
Alex Santos <alexandre.santos@publico.pt>
2024-03-09 17:20:27 +0000

    add telegraph

---
+++ headline--telegraph.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.telegraph.co.uk/us';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url, { timeout: 90000 }).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('article.card .card__textContent h2.list-headline', 'article.card .card__textContent h2.list-headline > a', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articleTitle = $('article.card .card__textContent h2.list-headline');
+    const articleUrl = $('article.card .card__textContent h2.list-headline > a');
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.first().text().trim(),
+      url: 'https://www.telegraph.co.uk' + articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "The Telegraph",
+    }));
+
+    fs.writeFile('headlines-world/headline--telegraph.json', jsonString, function(err){
+      console.log('File successfully written');
+      process.exit(0);
+    });
+    
+    console.log(jsonString);
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
