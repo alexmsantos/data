e3404443f19b75a8a878866c2e30e84164492a1b
Alex Santos <alexandre.santos@publico.pt>
2024-03-09 09:32:44 +0000

    refactor rr

--- renascencaHeadline.js
+++ renascencaHeadline.js
@@ -7,20 +7,23 @@ rp(url)
   .then(function(html){
     const $ = cheerio.load(html);
 
-    const articles = [];
     const articleTitle = $('.column-group.bottom-space a h3.superFix', html).eq(0);
     const articleUrl = $('.column-group.bottom-space .all-100:last-child a');
-    articles.push(articleTitle.text().trim());
-    articles.push('https://rr.sapo.pt/' + articleUrl[0].attribs.href);
     let ms = new Date();
     const dateIso = ms.toISOString()
-    articles.push(dateIso);
-    console.log(articles);
 
-    const jsonString = JSON.stringify(Object.assign({}, articles))
-    fs.writeFile('headline--renascenca.json', jsonString, function(err){
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.text().trim(),
+      url: 'https://rr.sapo.pt/' + articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "Renascença"
+    }));
+
+    fs.writeFile('headlines/headline--renascenca.json', jsonString, function(err){
       console.log('File successfully written');
     });
+
+    console.log(jsonString);
 
   })
   .catch(function(err){
