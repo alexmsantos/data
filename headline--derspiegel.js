c7ac212ba20b12872b61d087bf483eaf0e52e350
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 13:11:20 +0000

    add derspiegel

---
+++ headline--derspiegel.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.spiegel.de';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url, { timeout: 90000 }).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('main section article header h2 > a span:nth-child(2)', 'main section article header h2 > a', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articleTitle = $('main section article header h2 > a span:nth-child(2)');
+    const articleUrl = $('main section article header h2 > a');
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.first().text().trim(),
+      url: articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "Der Spiegel",
+    }));
+
+    fs.writeFile('headlines-world/headline--derspiegel.json', jsonString, function(err){
+      console.log('File successfully written');
+      process.exit(0);
+    });
+    
+    console.log(jsonString);
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
