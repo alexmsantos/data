b53a15aac103edc612bc96690969eb9bb6ef2fb0
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 16:09:49 +0000

    add reuters and fix bbc

--- headline--reuters.js
+++ headline--reuters.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const cheerio = require('cheerio');
-const url = 'https://www.bbc.com/news';
+const url = 'https://www.reuters.com';
 
 puppeteer
   .launch()
@@ -13,7 +13,7 @@ puppeteer
     page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36');
     return page.goto(url, { timeout: 90000 }).then(function() {
       // Wait for the dynamic content to load
-      return page.waitForSelector('main ul li a > span > p > span', 'main ul li a', { timeout: 20000 });
+      return page.waitForSelector('#main-content ul > li > div > a > span', '#main-content ul > li > div > a', { timeout: 20000 });
     })
     .then(function() {
       return page.content();
@@ -21,19 +21,19 @@ puppeteer
   })
   .then(function(html) {
     const $ = cheerio.load(html);
-    const articleTitle = $('main ul li a > span > p > span');
-    const articleUrl = $('main ul li a');
+    const articleTitle = $('#main-content ul > li > div > a > span');
+    const articleUrl = $('#main-content ul > li > div > a');
     let ms = new Date();
     const dateIso = ms.toISOString()
 
     const jsonString = JSON.stringify(Object.assign({}, {
       title: articleTitle.first().text().trim(),
-      url: 'https://www.bbc.com' + articleUrl[0].attribs.href,
+      url: 'https://www.reuters.com' + articleUrl[0].attribs.href,
       fetchDate: dateIso,
-      media: "BBC News",
+      media: "Reuters",
     }));
 
-    fs.writeFile('headlines-world/headline--bbc.json', jsonString, function(err){
+    fs.writeFile('headlines-world/headline--reuters.json', jsonString, function(err){
       console.log('File successfully written');
       process.exit(0);
     });
