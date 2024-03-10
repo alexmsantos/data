448deb69f3991c48df21cbbda149d9da3a9206ea
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 13:49:03 +0000

    add aljazeera

---
+++ headline--aljazeera.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.aljazeera.com';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url, { timeout: 90000 }).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('main article .article-card__title', 'main article .u-clickable-card__link', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articleTitle = $('main article .article-card__title');
+    const articleUrl = $('main article .u-clickable-card__link');
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.first().text().trim(),
+      url: 'https://www.aljazeera.com' + articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "Al Jazeera",
+    }));
+
+    fs.writeFile('headlines-world/headline--aljazeera.json', jsonString, function(err){
+      console.log('File successfully written');
+      process.exit(0);
+    });
+    
+    console.log(jsonString);
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
