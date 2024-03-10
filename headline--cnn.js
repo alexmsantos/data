7518fa121d2c768907a68ea2099a3178ec0f8366
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 16:45:29 +0000

    Update headline--cnn.js

--- headline--cnn.js
+++ headline--cnn.js
@@ -11,9 +11,9 @@ puppeteer
   .then(function(page) {
     // Define the user agent
     page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36');
-    return page.goto(url, { timeout: 90000 }).then(function() {
+    return page.goto(url, { timeout: 120000 }).then(function() {
       // Wait for the dynamic content to load
-      return page.waitForSelector('.container_lead-package__cards-wrapper .container__headline-text', '.container_lead-package__cards-wrapper a.container_lead-package__link', { timeout: 60000 });
+      return page.waitForSelector('.container_lead-package__cards-wrapper .container__headline-text', '.container_lead-package__cards-wrapper a.container_lead-package__link', { timeout: 90000 });
     })
     .then(function() {
       return page.content();
