5a430b55657885e196a23634f389d8a60529db7c
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 13:41:55 +0000

    Update headline--derspiegel.js

--- headline--derspiegel.js
+++ headline--derspiegel.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const cheerio = require('cheerio');
-const url = 'https://www.spiegel.de';
+const url = 'https://www.spiegel.de/international/';
 
 puppeteer
   .launch()
