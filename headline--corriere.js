d5cbf9bc8ce5a9ec4995b3a700d33322248dfe00
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 12:31:31 +0000

    add corriere

---
+++ headline--corriere.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.corriere.it';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url, { timeout: 90000 }).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('.body-hp .media-news__content .title-art-hp > a:first-of-type', '.body-hp .media-news__content .title-art-hp > a:first-of-type', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articleTitle = $('.body-hp .media-news__content .title-art-hp > a:first-of-type');
+    const articleUrl = $('.body-hp .media-news__content .title-art-hp > a:first-of-type');
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.first().text().trim(),
+      url: articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "Corriere Della Sera",
+    }));
+
+    fs.writeFile('headlines-world/headline--corriere.json', jsonString, function(err){
+      console.log('File successfully written');
+      process.exit(0);
+    });
+    
+    console.log(jsonString);
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
