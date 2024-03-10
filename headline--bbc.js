d1b15f767b5630125e6df7f13013b092c787f6c2
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 14:02:31 +0000

    add bbc news

---
+++ headline--bbc.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const url = 'https://www.bbc.com/news';
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url, { timeout: 90000 }).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('main ul li a p span', 'main ul li a', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    const $ = cheerio.load(html);
+    const articleTitle = $('main ul li a p span');
+    const articleUrl = $('main ul li a');
+    let ms = new Date();
+    const dateIso = ms.toISOString()
+
+    const jsonString = JSON.stringify(Object.assign({}, {
+      title: articleTitle.first().text().trim(),
+      url: 'https://www.bbc.com' + articleUrl[0].attribs.href,
+      fetchDate: dateIso,
+      media: "BBC News",
+    }));
+
+    fs.writeFile('headlines-world/headline--bbc.json', jsonString, function(err){
+      console.log('File successfully written');
+      process.exit(0);
+    });
+    
+    console.log(jsonString);
+
+  })
+  .catch(function(err) {
+    console.log(err);
+    process.exit(1);
+  });
