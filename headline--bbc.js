04328f95770544165c87f47057b83a1a8d1728fc
Alex Santos <alexandre.santos@publico.pt>
2024-03-10 17:45:58 +0000

    Update headline--bbc.js

--- headline--bbc.js
+++ headline--bbc.js
@@ -1,33 +1,19 @@
-const puppeteer = require('puppeteer');
+const rp = require('request-promise');
 const fs = require('fs');
 const cheerio = require('cheerio');
 const url = 'https://www.bbc.com/news';
 
-puppeteer
-  .launch({ headless: true, args: ['--no-sandbox'] })
-  .then(function(browser) {
-    return browser.newPage();
-  })
-  .then(function(page) {
-    // Define the user agent
-    page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36');
-    return page.goto(url, { timeout: 120000 }).then(function() {
-      // Wait for the dynamic content to load
-      return page.waitForSelector('main ul li a > span > p > span', 'main ul li a', { timeout: 60000 });
-    })
-    .then(function() {
-      return page.content();
-    });
-  })
-  .then(function(html) {
+rp(url)
+  .then(function(html){
     const $ = cheerio.load(html);
-    const articleTitle = $('main ul li a > span > p > span');
+
+    const articleTitle = $('main ul li a > span > p > span').eq(0);
     const articleUrl = $('main ul li a');
     let ms = new Date();
     const dateIso = ms.toISOString()
 
     const jsonString = JSON.stringify(Object.assign({}, {
-      title: articleTitle.first().text().trim(),
+      title: articleTitle.text().trim(),
       url: 'https://www.bbc.com' + articleUrl[0].attribs.href,
       fetchDate: dateIso,
       media: "BBC News",
@@ -37,11 +23,10 @@ puppeteer
       console.log('File successfully written');
       process.exit(0);
     });
-    
+
     console.log(jsonString);
 
   })
-  .catch(function(err) {
+  .catch(function(err){
     console.log(err);
-    process.exit(1);
   });
