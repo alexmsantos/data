817e904d83753283e56a338b518253a45d975699
Alex Santos <alexmiguelsantos@gmail.com>
2024-12-24 10:22:33 +0000

    Update cmHeadline.js

--- cmHeadline.js
+++ cmHeadline.js
@@ -1,31 +1,45 @@
-const rp = require('request-promise');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 const cheerio = require('cheerio');
 const url = 'https://www.cmjornal.pt';
 
-rp(url)
-  .then(function(html){
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url, { timeout: 90000 }).then(function() {
+      // Wait for the dynamic content to load
+      return page.waitForSelector('article.destaque .text_container a:nth-of-type(2) > h2', 'article.destaque .text_container a:nth-of-type(2)', { timeout: 20000 });
+    })
+    .then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
     const $ = cheerio.load(html);
-
-    const articleTitle = $('article.destaque .text_container a:nth-of-type(2) > h2', html).eq(0);
+    const articleTitle = $('article.destaque .text_container a:nth-of-type(2) > h2');
     const articleUrl = $('article.destaque .text_container a:nth-of-type(2)');
     let ms = new Date();
     const dateIso = ms.toISOString()
 
     const jsonString = JSON.stringify(Object.assign({}, {
-      title: articleTitle.text().trim(),
+      title: articleTitle.first().text().trim(),
       url: 'https://www.cmjornal.pt' + articleUrl[0].attribs.href,
       fetchDate: dateIso,
-      media: "Correio da Manhã"
+      media: "Correio da Manhã",
     }));
 
     fs.writeFile('headlines/headline--cm.json', jsonString, function(err){
       console.log('File successfully written');
+      process.exit(0);
     });
-
+    
     console.log(jsonString);
 
   })
-  .catch(function(err){
+  .catch(function(err) {
     console.log(err);
+    process.exit(1);
   });
