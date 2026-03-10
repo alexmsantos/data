d45607a966bbca63d9e5176b149f33bdd9d1dc7d
Alex Santos <alexmiguelsantos@gmail.com>
2026-03-10 10:55:34 +0000

    update rr

--- rss-to-json--renascenca.js
+++ rss-to-json--renascenca.js
@@ -1,16 +1,32 @@
 const { parse } = require('rss-to-json');
 const fs = require('fs');
+const path = require('path');
+
 // async await
 (async () => {
+    try {
+        console.log('Starting RSS fetch from https://rr.pt/rssfeed-ultimas');
+        
+        var rss = await parse('https://rr.pt/rssfeed-ultimas', {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (compatible; DataScraper/1.0)'
+            }
+        });
 
-    var rss = await parse('https://rr.pt/rssfeed-ultimas');
+        let jsonStringify = JSON.stringify(rss, null, 3);
 
-    let jsonStringify = JSON.stringify(rss, null, 3);
+        console.log(jsonStringify);
 
-    console.log(jsonStringify);
-
-    fs.writeFile('renascenca.json', jsonStringify, function(err){
-      console.log('File successfully written');
-    });
-
+        const filePath = path.join(__dirname, 'renascenca.json');
+        fs.writeFile(filePath, jsonStringify, function(err){
+            if (err) {
+                console.error('Error writing file:', err);
+                process.exit(1);
+            }
+            console.log('File successfully written to:', filePath);
+        });
+    } catch (error) {
+        console.error('Error fetching RSS:', error.message);
+        process.exit(1);
+    }
 })();
